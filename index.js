1c4f368d01b84dee63769a8ac46aed59852d1242
ArtemZayarny <artyom.zayarny@gmail.com>
2023-10-05 22:27:54 +0300

    added create tour route

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
 );
@@ -12,6 +14,24 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     tours,
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: newTour,
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
