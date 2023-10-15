915d665c103ce1ef04ebbd49cf88174b774bdcda
ArtemZayarny <artyom.zayarny@gmail.com>
2023-10-15 18:43:05 +0300

    modeling tour guide reference

--- models/tourModel.js
+++ models/tourModel.js
@@ -103,7 +103,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     toJSON: { virtuals: true },
@@ -127,13 +127,13 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
 
-  this.guides = await Promise.all(guidesPromises);
+//   this.guides = await Promise.all(guidesPromises);
 
-  next();
-});
+//   next();
+// });
 
 // Multiple pre or post middleware
 tourSchema.pre('save', function (next) {
