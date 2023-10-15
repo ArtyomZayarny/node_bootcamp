db249559c1c22082c12e62fa8633d19dcf36e421
ArtemZayarny <artyom.zayarny@gmail.com>
2023-10-15 18:12:43 +0300

    embeding guied for tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -102,6 +103,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -122,6 +124,14 @@ tourSchema.virtual('durationWeeks').get(function () {
 // Document on pre - runs  before event - .save()   .create() | !not insertMany()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+
+  this.guides = await Promise.all(guidesPromises);
+
   next();
 });
 
