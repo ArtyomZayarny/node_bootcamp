a46be40047b1e8bb2ac325e30c6c3be2c5801a29
ArtemZayarny <artyom.zayarny@gmail.com>
2023-10-28 19:41:44 +0300

    importing review and user data

--- dev-data/import-dev-data.js
+++ dev-data/import-dev-data.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
 const Tour = require('../models/tourModel');
+const Review = require('../models/reviewModel');
+const User = require('../models/userModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -21,11 +23,17 @@ mongoose
 
 // Read JSON File
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 
 //Import Data into DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('✅ Data is loaded succesfull');
   } catch (error) {
     console.log(error);
@@ -37,6 +45,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('✅ Data is deleted successfully');
   } catch (error) {
     console.log(error);
